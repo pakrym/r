77a6563e329b87f92e3fffc1205240ae674e3def
Pavel Krymets <pavel@krymets.com>
2017-01-03 15:52:13 -0800

    Span corruption repro

---
+++ src/SpanCorrupt/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+using Xunit;
+
+namespace SpanCorrupt
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            GetValue2();
+        }
+
+        [Fact]
+        private static void GetValue2()
+        {
+            W(Encoding.ASCII.GetBytes("Header:value\r\n\r\n"));
+        }
+
+        private static void W(Span<byte> source)
+        {
+            Console.WriteLine("BEFORE LOOP " + string.Join(" ", source.ToArray()));
+            for (int i = 1; i < 32 ; i ++)
+            {
+                var a = Environment.StackTrace;
+
+                Console.WriteLine(i + " -- " +string.Join(" ", source.ToArray()));
+            }
+        }
+    }
+}
